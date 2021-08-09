9d045c10c03b70bb9869672851546892d71b4666
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-08-09 16:07:29 -0300

    starting docstrings

--- checker/util.py
+++ checker/util.py
@@ -5,12 +5,34 @@ import pandas as pd
 import datetime
 
 today = datetime.datetime.today()
+delta = datetime.datetime.timedelta(days=1)
 
-def fetch_profiles(tlim=(today-1, today), days_ago=None, ndays=None):
+def fetch_sbd(tlim=(today-delta, today), days_ago=None, ndays=None):
+    '''
+    Function to fetch a list of profiles from the Argo Canada google drive, 
+    including their metadata, for a given time period defined by either two
+    delimiting datetimes, or a number of days ago and number of days from that
+    time to be included.
 
-    return None
-
-def expected_profiles(tlim=(today-1, today), days_ago=None, ndays=None):
-
-    return None
+    Args:
+        - tlim (tuple): tuple of two datestrings or datetimes used to select files
+        - days_ago (float): number of days ago to get files, can be float
+        - ndays (float): number of days from `days_ago` to get files
     
+    Returns:
+    '''
+
+
+    return None
+
+def expected_profiles(tlim=(today-delta, today), days_ago=None, ndays=None):
+    '''
+    Function that looks at the global Argo index for a given DAC (note - set
+    up currently has MEDS as default, but could be adapted for any DAC), and
+    given a float cycling period, returns a list of floats expected return
+    a new profile within a specified date range. 
+
+    Args: 
+    '''
+
+    return None
