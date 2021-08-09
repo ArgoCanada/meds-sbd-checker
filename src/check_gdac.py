10ef61903ce2a76ae2c7b3fdfa6765a361910743
Chris Gordon <chris.gordon@dfo-mpo.gc.ca>
2021-08-09 16:18:47 -0300

    comments

--- src/check_gdac.py
+++ src/check_gdac.py
@@ -3,13 +3,17 @@
 from oauth2client.service_account import ServiceAccountCredentials
 from googleapiclient.discovery import build
 
+# google drive API v3 setup
 scope = 'https://www.googleapis.com/auth/drive'
 credentials = ServiceAccountCredentials.from_json_keyfile_name('token.json', scope)
 service = build('drive', 'v3', credentials=credentials)
 
-items = []
-page_token = None
+# loop variables
+items = [] # list of file dicts
+page_token = None # says whether or not we have reached the last page
 
+# get all files in sbd folder, break when last page is reached, but page_token
+# is None to start and end so can't build condition on that
 while True:
     results = service.files().list(spaces='drive', fields='nextPageToken, files(id, name, createdTime)', pageToken=page_token).execute()
     items = items + results.get('files', [])
