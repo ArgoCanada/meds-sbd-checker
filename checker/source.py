133c8469d8b06ede116a1ec37ca039d1b93b3574
Dewey Dunnington <dewey.dunnington@dfo-mpo.gc.ca>
2021-08-12 17:05:57 -0300

    working gmail checker!

--- checker/source.py
+++ checker/source.py
@@ -1,6 +1,7 @@
 
 from typing import BinaryIO, Iterable, Tuple
 from datetime import datetime
+import base64
 import io
 import googleapiclient.discovery
 
@@ -38,9 +39,32 @@ class GoogleDriveDataSource(RawFloatDataSource):
     :param folder_id: The shared folder identifier. Should look
         like this: 2bhGbTy9G7HJnSEAsHS3p9CNwYGwvYdAX.
     :param credentials: A ``from oauth2client.service_account.ServiceAccountCredentials``
-        used for authentication. Not necessary if the drive is public.
+        used for authentication. Not necessary if the GOOGLE_APPLICATION_CREDENTIALS
+        environment variable has been set.
     :param args: Further arguments to the /files/list endpoint.
     """
+
+    class LazyDriveFile:
+        """Lazy wrapper that doesn't download unless :meth:`read` is called"""
+
+        def __init__(self, service, file_id) -> None:
+            self._service = service
+            self._file_id = file_id
+            self._buffer = None
+        
+        def read(self, size=None):
+            if self._buffer is None:
+                # this will download the file all at once which is OK for now
+                req = self._service.files().get_media(fileId=self._file_id)
+                self._buffer = io.BytesIO(req.execute())
+            return self._buffer.read(size)
+        
+        def __enter__(self):
+            return self
+        
+        def __exit__(self, *execinfo):
+            pass
+                
 
     def __init__(self, folder_id, credentials=None, args=None) -> None:
         self._service = googleapiclient.discovery.build('drive', 'v3', credentials=credentials)
@@ -57,7 +81,6 @@ class GoogleDriveDataSource(RawFloatDataSource):
     
     def __iter__(self) -> Iterable[Tuple[str, datetime, BinaryIO]]:    
         page_token = None  # says whether or not we have reached the last page
-        sfields = 'id, name, createdTime'  # string list of fields to fetch
 
         # get all files in folder, break when last page is reached, but page_token
         # is None to start and end so can't build condition on that
@@ -72,12 +95,59 @@ class GoogleDriveDataSource(RawFloatDataSource):
 
             # iterate through items
             for item in results.get('files', []):
+                # TODO: specify UTC?
                 time = datetime.strptime(item['createdTime'], '%Y-%m-%dT%H:%M:%S.%fZ')
-                # this will download the file all at once which is OK for what we need
-                download_req = self._service.files().get_media(fileId=item['id'])
-                yield item['name'], time, io.BytesIO(download_req.execute())
+                with GoogleDriveDataSource.LazyDriveFile(self._service, item['id']) as file:
+                    yield item['name'], time, file
 
             # get more items if the user hasn't stopped iterating yet
             page_token = results.get('nextPageToken', None)
             if page_token is None:
+                break
+
+
+class GmailDataSource(RawFloatDataSource):
+
+    def __init__(self, credentials, max_messages=1000, args=None):
+        self._service = googleapiclient.discovery.build('gmail', 'v1', credentials=credentials)
+        self._args = {} if args is None else {}
+        self._max_messages = max_messages
+
+    def __iter__(self) -> Iterable[Tuple[str, datetime, BinaryIO]]:
+        page_token = None
+        n_messages = 0
+
+        while True:
+            # get messages
+            messages = self._service.users().messages().list(
+                userId='me',
+                **self._args,
+                pageToken=page_token
+            ).execute()
+
+            for msg_item in messages['messages']:
+                message = self._service.users().messages().get(
+                    userId='me',
+                    id=msg_item['id']
+                ).execute()
+
+                n_messages += 1
+
+                time = datetime.fromtimestamp(int(message['internalDate']) / 1000)
+                for part in message['payload']['parts']:
+                    if 'attachmentId' in part['body']:
+                        name = part['filename']
+                        attachment = self._service.users().messages().attachments().get(
+                            userId='me',
+                            messageId=msg_item['id'],
+                            id=part['body']['attachmentId']
+                        ).execute()
+
+                        data_base64 = attachment['data']
+                        data_bytes = base64.urlsafe_b64decode(data_base64)
+                        yield name, time, io.BytesIO(data_bytes)
+
+            # get more messages if we're not done iterating
+            page_token = messages.get('nextPageToken', None)
+            if page_token is None or n_messages >= self._max_messages:
                 break
--- tests/test_source.py
+++ tests/test_source.py
@@ -4,10 +4,53 @@ import os
 from datetime import datetime
 
 from oauth2client.service_account import ServiceAccountCredentials
+from google.oauth2.credentials import Credentials
 from checker import source
 
 
 class TestSource(unittest.TestCase):
+
+    def setUp(self) -> None:
+        # can use a service account credential for the Drive API
+        json_file = [f for f in os.listdir('.') if f.endswith('-drive.json')]
+        if not json_file:
+            self.skipTest('Google application credentials not found')
+
+        self.cred = ServiceAccountCredentials.from_json_keyfile_name(
+            json_file[0],
+            scopes=[
+                'https://www.googleapis.com/auth/drive.readonly'
+            ]
+        )
+
+        # gmail API needs a user authorization because it accesses
+        # an actual email account which in most cases is highly
+        # sensitive
+        from google_auth_oauthlib.flow import InstalledAppFlow
+        from google.auth.transport.requests import Request
+        from google.oauth2.credentials import Credentials
+
+        # https://developers.google.com/gmail/api/quickstart/python
+        scopes = ['https://www.googleapis.com/auth/gmail.readonly']
+        creds = None
+        if os.path.exists('token-gmail.json'):
+            creds = Credentials.from_authorized_user_file('token-gmail.json', scopes)
+        
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                creds.refresh(Request())
+            else:
+                flow = InstalledAppFlow.from_client_secrets_file(
+                    'client_secret-gmail.json', 
+                    scopes
+                )
+                creds = flow.run_local_server(port=53844)
+            
+            # Save the credentials for the next run
+            with open('token-gmail.json', 'w') as token:
+                token.write(creds.to_json())
+        
+        self.gmail_creds = creds
 
     def test_dummy(self):
         for name, time, f in source.TestRawFloatDataSource():
@@ -20,28 +63,28 @@ class TestSource(unittest.TestCase):
         # https://drive.google.com/drive/folders/1bhGbTy9G7HJnSEAsHS3p9CNwYGwvYdAY
         # it has exactly one file named test_file.txt with the content b'test content\r\n'
         dummy_id = '1bhGbTy9G7HJnSEAsHS3p9CNwYGwvYdAY'
-
-        # dev environment will have some-file.json, but cloud might have
-        # the Google-sanctioned way of doing this
-        if not os.getenv('GOOGLE_APPLICATION_CREDENTIALS'):
-            json_file = [f for f in os.listdir('.') if f.endswith('.json')]
-            if not json_file:
-                self.skipTest('Google application credentials not found')
-
-            cred = ServiceAccountCredentials.from_json_keyfile_name(
-                json_file[0],
-                scopes='https://www.googleapis.com/auth/drive.readonly'
-            )
-        else:
-            cred = None
-        
+        src = source.GoogleDriveDataSource(dummy_id, credentials=self.cred)
         n_files = 0
-        for name, time, f in source.GoogleDriveDataSource(dummy_id, credentials=cred):
+        for name, time, f in src:
             n_files += 1
             self.assertLessEqual(n_files, 1)
             self.assertEqual(name, 'test_file.txt')
             self.assertIsInstance(time, datetime)
             self.assertEqual(f.read(), b'test content\r\n')
+        
+    def test_gmail(self):
+        # this test assumes authentication against the argo.canada.gc@gmail.com email
+        src = source.GmailDataSource(credentials=self.gmail_creds)    
+        n_files = 0
+        for name, time, f in src:
+            n_files +=1
+
+            self.assertRegex(name, r'.sbd$')
+            self.assertIsInstance(time, datetime)
+            self.assertEqual(len(f.read()), 300)
+            
+            if n_files >= 5:
+                break
 
 
 if __name__ == '__main__':
